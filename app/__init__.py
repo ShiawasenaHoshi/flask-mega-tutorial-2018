7e469c059f0d6d98ee8ba8c0bd45a40c0870b339
Vasily <shiawasena.hoshi@gmail.com>
2019-04-16 22:50:24 +0800

    lesson 11 (only fantastic forms)

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,11 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 mail = Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
