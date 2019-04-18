092d192734c36b59b90aa4240217ee52b6a5a4a1
Vasily <shiawasena.hoshi@gmail.com>
2019-04-18 16:45:58 +0800

    lesson 15 (refactoring in process)

--- app/auth/email.py
+++ app/auth/email.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask_mail import Message
 from flask_babel import _
 from app import app, mail
+from app import current_app
 
 
 def send_async_email(app, msg):
@@ -14,7 +15,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
+    Thread(target=send_async_email, args=(current_app.get_current_object(), msg)).start()
 
 
 def send_password_reset_email(user):
